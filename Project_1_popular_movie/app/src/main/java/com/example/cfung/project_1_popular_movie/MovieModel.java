f4f013f446cca8b8d2a5a8c83ab0c6535a843b41
cfung <chesterfung@gmail.com>
2017-12-19 17:01:39 -0800

    add popularity to movie model

--- Project_1_popular_movie/app/src/main/java/com/example/cfung/project_1_popular_movie/MovieModel.java
+++ Project_1_popular_movie/app/src/main/java/com/example/cfung/project_1_popular_movie/MovieModel.java
@@ -7,16 +7,19 @@ package com.example.cfung.project_1_popular_movie;
 public class MovieModel {
 
     private String movieName;
+    private String popularity;
     private String movieLink;
     //private int image; // drawable reference id
 
-    public MovieModel(String mName, String mLink)
+    public MovieModel(String mName, String poplularity, String mLink)
     {
         this.movieName = mName;
+        this.popularity = poplularity;
         this.movieLink = mLink;
     }
 
     public String getMovieName(){ return this.movieName;}
+    public String getPopularity(){ return this.popularity;}
     public String getMovieLink(){return this.movieLink;}
 
 }
