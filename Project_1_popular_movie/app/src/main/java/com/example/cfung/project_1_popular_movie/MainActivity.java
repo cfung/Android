9738eb6c6c8c2a131f0db6534dcef7a65b9ac2ee
cfung <chesterfung@gmail.com>
2017-12-27 18:13:44 -0800

    add actionbar and menu

--- Project_1_popular_movie/app/src/main/java/com/example/cfung/project_1_popular_movie/MainActivity.java
+++ Project_1_popular_movie/app/src/main/java/com/example/cfung/project_1_popular_movie/MainActivity.java
@@ -8,6 +8,7 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -143,6 +144,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        Log.v(TAG, "inside onCreate");
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        if (toolbar == null){
+            throw new Error("canot find toolbar..");
+        }
+
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
 
         GridView gridView = (GridView) findViewById(R.id.movie_grid);
         AllMovies = new ArrayList<MovieModel>();
@@ -156,7 +167,9 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        Log.v(TAG,"inside onCreateOptionsMenu..");
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
         return true;
     }
 
@@ -167,11 +180,16 @@ public class MainActivity extends AppCompatActivity {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        switch (id) {
+            case R.id.action_popular:
+                return true;
+
+            case R.id.action_toprated:
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
         }
 
-        return super.onOptionsItemSelected(item);
     }
 }
