ff08aee115a18adda8c1a0a9140b10f3e77ac512
cfung <chesterfung@gmail.com>
2017-12-27 23:46:28 -0800

    init DetailActivity and xml

---
+++ Project_1_popular_movie/app/src/main/java/com/example/cfung/project_1_popular_movie/DetailActivity.java
@@ -0,0 +1,18 @@
+package com.example.cfung.project_1_popular_movie;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by cfung on 12/27/17.
+ */
+
+public class DetailActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.detail);
+
+    }
+}
