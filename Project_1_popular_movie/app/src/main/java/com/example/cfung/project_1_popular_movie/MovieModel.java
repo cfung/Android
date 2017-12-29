47516ca52c9e2df50ed77ed042f196fbe2f0c2e0
cfung <chesterfung@gmail.com>
2017-12-28 23:27:34 -0800

    add more members to MovieModel

--- Project_1_popular_movie/app/src/main/java/com/example/cfung/project_1_popular_movie/MovieModel.java
+++ Project_1_popular_movie/app/src/main/java/com/example/cfung/project_1_popular_movie/MovieModel.java
@@ -9,17 +9,26 @@ public class MovieModel {
     private String movieName;
     private String popularity;
     private String movieLink;
+    private String overview;
+    private String vote_average;
+    private String release_date;
     //private int image; // drawable reference id
 
-    public MovieModel(String mName, String poplularity, String mLink)
+    public MovieModel(String mName, String poplularity, String mLink, String mOverview, String mVote_average, String mRelease_date)
     {
         this.movieName = mName;
         this.popularity = poplularity;
         this.movieLink = mLink;
+        this.overview = mOverview;
+        this.vote_average = mVote_average;
+        this.release_date = mRelease_date;
     }
 
     public String getMovieName(){ return this.movieName;}
     public String getPopularity(){ return this.popularity;}
     public String getMovieLink(){return this.movieLink;}
+    public String getOverview(){return this.overview;}
+    public String getVote_average(){return this.vote_average;}
+    public String getRelease_date(){return this.release_date;}
 
 }
