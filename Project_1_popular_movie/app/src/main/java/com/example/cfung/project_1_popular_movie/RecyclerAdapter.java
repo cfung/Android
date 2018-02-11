600321f8fbd47d1ae02ca375e56f6c43b768cb97
cfung <chesterfung@gmail.com>
2018-02-10 23:42:48 -0800

    add template for RecyclerView for reviews

---
+++ Project_1_popular_movie/app/src/main/java/com/example/cfung/project_1_popular_movie/RecyclerAdapter.java
@@ -0,0 +1,28 @@
+package com.example.cfung.project_1_popular_movie;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
+
+/**
+ * Created by cfung on 2/10/18.
+ */
+
+public class RecyclerAdapter extends RecyclerView.Adapter {
+
+
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
