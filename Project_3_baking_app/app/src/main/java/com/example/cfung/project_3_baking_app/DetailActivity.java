a8afa26d717faee31fff9fa93b380c0d535d264a
Chester Fung <chesterfung@gmail.com>
2018-03-05 10:55:20 -0800

    create and declare new DetailActivity in manifest.xml

---
+++ Project_3_baking_app/app/src/main/java/com/example/cfung/project_3_baking_app/DetailActivity.java
@@ -0,0 +1,10 @@
+package com.example.cfung.project_3_baking_app;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by cfung on 3/5/18.
+ */
+
+public class DetailActivity extends AppCompatActivity{
+}
