b74b7628abae9c5fde96076deba6bb9b7597dccd
Chester Fung <chesterfung@gmail.com>
2018-10-02 22:37:55 -0700

    add TextView for various fields from sandwich JSON return

--- sandwich-club/app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
+++ sandwich-club/app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
@@ -3,7 +3,9 @@ package com.udacity.sandwichclub;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
@@ -18,6 +20,16 @@ public class DetailActivity extends AppCompatActivity {
 
     public static final String EXTRA_POSITION = "extra_position";
     private static final int DEFAULT_POSITION = -1;
+    private static final String TAG = DetailActivity.class.getSimpleName();
+
+    TextView origin_tv = findViewById(R.id.origin_tv);
+    TextView also_known_as_text = findViewById(R.id.also_known_as_text);
+    TextView ingredients_text = findViewById(R.id.ingredients_text);
+    TextView place_of_origin_text = findViewById(R.id.place_of_origin_text);
+    TextView description_tv = findViewById(R.id.description_tv);
+    TextView ingredients_tv = findViewById(R.id.ingredients_tv);
+    TextView also_known_tv = findViewById(R.id.also_known_tv);
+    TextView description_text = findViewById(R.id.description_text);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +37,7 @@ public class DetailActivity extends AppCompatActivity {
         setContentView(R.layout.activity_detail);
 
         ImageView ingredientsIv = findViewById(R.id.image_iv);
+
 
         Intent intent = getIntent();
         if (intent == null) {
@@ -49,11 +62,11 @@ public class DetailActivity extends AppCompatActivity {
                 closeOnError();
                 return;
             }
-            populateUI();
+            populateUI(sandwich);
             Picasso.with(this)
                     .load(sandwich.getImage())
                     .into(ingredientsIv);
-
+            Log.v(TAG, "getMainName: " + sandwich.getMainName());
             setTitle(sandwich.getMainName());
 
         }catch (Exception e){
@@ -67,7 +80,16 @@ public class DetailActivity extends AppCompatActivity {
         Toast.makeText(this, R.string.detail_error_message, Toast.LENGTH_SHORT).show();
     }
 
-    private void populateUI() {
+    private void populateUI(Sandwich sandwich) {
+
+        origin_tv.setText(sandwich.getPlaceOfOrigin());
+        //also_known_as_text.setText(sandwich.getAlsoKnownAs());
+        //ingredients_text.setText(sandwich.getIngredients());
+        place_of_origin_text.setText(sandwich.getPlaceOfOrigin());
+        description_tv.setText(sandwich.getDescription());
+        //ingredients_tv.setText(sandwich.getIngredients());
+        //also_known_tv.setText(sandwich.getAlsoKnownAs());
+        description_text.setText(sandwich.getDescription());
 
     }
 }
