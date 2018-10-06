2d4b578cfd7ba076aac38770182d22d113dbbc53
sk <chesterfung@gmail.com>
2018-10-05 23:06:17 -0700

    parse igredients and alsoknownasJSONArray

--- sandwich-club/app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ sandwich-club/app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -4,9 +4,11 @@ import android.util.Log;
 
 import com.udacity.sandwichclub.model.Sandwich;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class JsonUtils {
@@ -27,21 +29,32 @@ public class JsonUtils {
         JSONObject sandwichJson = new JSONObject(json);
         JSONObject name = sandwichJson.getJSONObject("name");
         String mainName = name.getString("mainName");
-        List<String> alsoKnownAs = null;
-        /*for (int i=0; i < name.getJSONArray("alsoKnownAs").length(); i++){
-            alsoKnownAs.add(name.getJSONArray("alsoKnownAs")[i]);
+        JSONArray alsoKnownAs = name.getJSONArray("alsoKnownAs");
+        JSONArray ingredients = sandwichJson.getJSONArray("ingredients");
+        List<String> alsoKnownAsList = new ArrayList<String>();
+        List<String> ingredientsList = new ArrayList<String>();
+        for (int i=0; i < alsoKnownAs.length(); i++){
 
-        }*/
+            alsoKnownAsList.add(alsoKnownAs.get(i).toString());
+
+        }
         String placeOfOrigin = sandwichJson.getString("placeOfOrigin");
         String description = sandwichJson.getString("description");
         String image = sandwichJson.getString("image");
 
+        for (int i=0; i < ingredients.length(); i++){
+
+            ingredientsList.add(ingredients.get(i).toString());
+
+        }
 
         Log.v(TAG, "json name:" + name);
         Log.v(TAG, "placeOfOrigin:" + placeOfOrigin);
         Log.v(TAG, "description:" + description);
         Log.v(TAG, "image:" + image);
-        Sandwich sandwich = new Sandwich(mainName, null, placeOfOrigin, description, image, null);
+        Log.v(TAG, "alsoknownAs:" + alsoKnownAsList);
+        Log.v(TAG, "ingredients:" + ingredientsList);
+        Sandwich sandwich = new Sandwich(mainName, alsoKnownAsList, placeOfOrigin, description, image, ingredientsList);
 
         Log.v(TAG, "Json " + json);
 
