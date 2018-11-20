57eee64116acb305c51225803b902943964e9cdd
cfung <chesterfung@gmail.com>
2018-11-20 12:55:51 -0800

    add MovieDao class

---
+++ Project_1_popular_movie/app/src/main/java/com/example/cfung/project_1_popular_movie/data/MovieDao.java
@@ -0,0 +1,33 @@
+package com.example.cfung.project_1_popular_movie.data;
+
+import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Delete;
+import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.OnConflictStrategy;
+import android.arch.persistence.room.Query;
+import android.arch.persistence.room.Update;
+
+import com.example.cfung.project_1_popular_movie.MovieModel;
+
+import java.util.ArrayList;
+
+/**
+ * Created by cfung on 11/20/18.
+ */
+
+@Dao
+public interface MovieDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    void InsertMovie();
+
+    @Query("SELECT * FROM " + MovieContract.MovieEntry.TABLE_NAME)
+    ArrayList<MovieModel> getFavoriteMoviesFromDB();
+
+    //@Update
+
+
+    @Delete
+    void DeleteMovie(MovieModel movieModel);
+
+}
