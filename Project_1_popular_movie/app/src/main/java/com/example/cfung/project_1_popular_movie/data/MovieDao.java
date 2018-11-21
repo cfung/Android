ca3506451e638691ccd42905251525a4c8453379
cfung <chesterfung@gmail.com>
2018-11-20 17:04:53 -0800

    modify InsertMovie params

--- Project_1_popular_movie/app/src/main/java/com/example/cfung/project_1_popular_movie/data/MovieDao.java
+++ Project_1_popular_movie/app/src/main/java/com/example/cfung/project_1_popular_movie/data/MovieDao.java
@@ -20,7 +20,7 @@ import java.util.ArrayList;
 public interface MovieDao {
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    void InsertMovie();
+    void InsertMovie(MovieModel movieModel);
 
     @Query("SELECT * FROM " + MovieContract.MovieEntry.TABLE_NAME)
     ArrayList<MovieModel> getFavoriteMoviesFromDB();
