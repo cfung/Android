bd4b7c5038cbf05245d0804810089ec8fb1d6030
cfung <chesterfung@gmail.com>
2018-11-27 23:21:11 -0800

    change return type of LiveData<List<MovieModel>>

--- Project_1_popular_movie/app/src/main/java/com/example/cfung/project_1_popular_movie/AddMovieViewModel.java
+++ Project_1_popular_movie/app/src/main/java/com/example/cfung/project_1_popular_movie/AddMovieViewModel.java
@@ -6,17 +6,19 @@ import android.view.View;
 
 import com.example.cfung.project_1_popular_movie.data.AppDatabase;
 
+import java.util.List;
+
 /**
  * Created by cfung on 11/26/18.
  */
 
 public class AddMovieViewModel extends ViewModel {
 
-    private LiveData<MovieModel> movie;
+    private LiveData<List<MovieModel>> movies;
 
     public AddMovieViewModel(AppDatabase database) {
-        movie = database.movieDao().getFavoriteMoviesFromDB();
+        movies = database.movieDao().getFavoriteMoviesFromDB();
     }
 
-    public LiveData<MovieModel> getMovie() { return movie; }
+    public LiveData<List<MovieModel>> getMovies() { return movies; }
 }
