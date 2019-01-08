cdb2275c51e6aba4575cf57b8d70fabf30125ea3
cfung <chesterfung@gmail.com>
2019-01-08 11:19:27 -0800

    extends Fragment

--- Project_3_baking_app/app/src/main/java/com/example/cfung/project_3_baking_app/RecipeStepDetailFragment.java
+++ Project_3_baking_app/app/src/main/java/com/example/cfung/project_3_baking_app/RecipeStepDetailFragment.java
@@ -1,8 +1,10 @@
 package com.example.cfung.project_3_baking_app;
 
+import android.app.Fragment;
+
 /**
  * Created by cfung on 1/7/19.
  */
 
-public class RecipeStepDetailFragment {
+public class RecipeStepDetailFragment extends Fragment{
 }
