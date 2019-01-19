e3452347ef7ddc42a2199a821f5bec8c345b93df
cfung <chesterfung@gmail.com>
2019-01-18 16:36:10 -0800

    override onSaveInstanceState

--- Project_3_baking_app/app/src/main/java/com/example/cfung/project_3_baking_app/MainActivity.java
+++ Project_3_baking_app/app/src/main/java/com/example/cfung/project_3_baking_app/MainActivity.java
@@ -37,13 +37,12 @@ public class MainActivity extends AppCompatActivity implements RecipeAdapter.Lis
     ArrayList<RecipeModel> AllRecipes = null;
     ArrayList<RecipeModel> result = null;
 
-    /*
-    @Override
-    public void onItemClick(View view, int position) {
-
-        Toast.makeText(this, "You clicked " + recipeAdapter.getItem(position) + " on row number " + position, Toast.LENGTH_SHORT).show();
-
-    }*/
+    static String RECIPES = "RECIPES";
+    static String SELECTED_RECIPES = "SELECTED_RECIPES";
+    static String STEPS = "STEPS";
+    static String INDEX = "INDEX";
+    static String RECIPE_DETAIL = "RECIPE_DETAIL";
+    static String RECIPE_STEP_DETAIL = "RECIPE_STEP_DETAIL";
 
     @Override
     public Loader<ArrayList<RecipeModel>> onCreateLoader(int i, Bundle bundle){
@@ -162,174 +161,28 @@ public class MainActivity extends AppCompatActivity implements RecipeAdapter.Lis
 
     }
 
-
-    // AsyncTask to perform network operation in a separate thread than mainUI thread
-    /*public class RecipeQueryTask extends AsyncTask<String, Void, ArrayList<RecipeModel>> {
-
-        @Override
-        protected ArrayList<RecipeModel> doInBackground(String... urls) {
-
-            String response = null;
-            ArrayList<RecipeModel> resultslist = new ArrayList<RecipeModel>();
-
-            try {
-
-                Log.v(TAG, "inside try of doInbackground..");
-
-                URL url = new URL(NetworkUtils.BAKING_APP_JSON);
-
-                response = NetworkUtils.getResponseFromHttpUrl(url);
-
-                JSONArray recipeResults = new JSONArray(response);
-
-                //JSONArray recipeResults = results.getJSONArray("");
-                Log.v(TAG, "what is size of recipeResutls..." + recipeResults.length());
-                for (int i = 0; i < recipeResults.length(); i++){
-                    //ArrayList<String> movieArray = new ArrayList<String>();
-                    JSONObject jsonobject = recipeResults.getJSONObject(i);
-                    int id = jsonobject.getInt("id");
-                    String name = jsonobject.getString("name");
-                    int servings = jsonobject.getInt("servings");
-                    String image = jsonobject.getString("image");
-                    ArrayList<Ingredient> ingredientsList = new ArrayList<>();
-                    JSONArray ingredients = jsonobject.getJSONArray("ingredients");
-
-                    for (int x = 0; x < ingredients.length(); x++){
-                        JSONObject json = ingredients.getJSONObject(x);
-                        int quantity = json.getInt("quantity");
-                        String measure = json.getString("measure");
-                        String ingredientStr = json.getString("ingredient");
-
-                        Ingredient ingredient = new Ingredient(quantity, measure, ingredientStr);
-                        ingredientsList.add(ingredient);
-
-                    }
-
-                    ArrayList<Steps> stepsList = new ArrayList<>();
-                    JSONArray steps = jsonobject.getJSONArray("steps");
-
-                    for (int y = 0; y < steps.length(); y++){
-
-                        JSONObject jsonStep = steps.getJSONObject(y);
-
-                        //ArrayList<RecipeModel.Steps> step = new ArrayList<>();
-
-                        int stepId = jsonStep.getInt("id");
-                        //stepsList.add(Integer.toString(stepId));
-                        String shortDesc = jsonStep.getString("shortDescription");
-                        String desc = (jsonStep.getString("description"));
-                        String videoURL = jsonStep.getString("videoURL");
-                        String thumbnailURL = jsonStep.getString("thumbnailURL");
-                        Steps step = new Steps(stepId, shortDesc, desc, videoURL, thumbnailURL);
-                        stepsList.add(step);
-
-                    }
-
-                    ArrayList<String> mReview = new ArrayList<String>();
-                    String trailer = null;
-
-                    // completed:  add recipe recipeArray
-                    RecipeModel recipe = new RecipeModel(id, name, ingredientsList, stepsList, servings, image);
-
-                    resultslist.add(recipe);
-
-                }
-
-            } catch (IOException e){
-                e.printStackTrace();
-            } catch(JSONException e){
-                e.printStackTrace();
-            }
-
-            return resultslist;
-        }*/
-
-        /*
-        @Override
-        protected void onPostExecute(ArrayList<RecipeModel> result){
-            super.onPostExecute(result);
-
-            Log.v(TAG, "what is onPostExecute result: " + result);
-
-            if(result != null){
-
-                //recipeAdapter.clear();
-                for (int i=0; i<result.size();i++){
-                    Log.v(TAG, "what is result (onPostExecute)..." + result.get(i));
-                    //recipeAdapter.add(result.get(i));
-                    AllRecipes.add(result.get(i));
-                }
-
-                recipeAdapter = new RecipeAdapter(MainActivity.this, AllRecipes);
-                mainView = (RecyclerView) findViewById(R.id.recycler_fragment_body);
-                //recyclerView.setLayoutManager(new LinearLayoutManager(this));
-                mainView.setAdapter(recipeAdapter);
-
-            }
-        }*/
-
-    //}
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.v(TAG, "onCreate");
-        //GridView gridView = (GridView) findViewById(R.id.recipe_grid);
-        //RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recycler_fragment_body);
-
-        /*
-        mainView = (RecyclerView) findViewById(R.id.recipe_main_recycler);
-        LinearLayoutManager llm = new LinearLayoutManager(this);
-        llm.setOrientation(LinearLayoutManager.VERTICAL);
-        mainView.setLayoutManager(llm);
-        mainView.setAdapter(recipeAdapter);*/
-
-        Log.v(TAG, "starting initLoader..");
-        //getLoaderManager().initLoader(0, null, this).forceLoad();
-
-        //AllRecipes = new ArrayList<RecipeModel>();
-        //recipeAdapter = new RecipeAdapter(MainActivity.this, AllRecipes);
-
-        //new RecipeQueryTask().execute(NetworkUtils.BAKING_APP_JSON);
-
-
-        /*recyclerView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
-
-                String selectedItem = adapterView.getItemAtPosition(position).toString();
-
-                Class detailActivity = DetailActivity.class;
-
-                Context context = MainActivity.this;
-                Intent startDetailActivityIntent = new Intent(context, detailActivity);
-                startDetailActivityIntent.putExtra("recipe", AllRecipes.get(position));
-                //startDetailActivityIntent.putExtra("id", AllRecipes.get(position).getid());
-                //startDetailActivityIntent.putExtra("name", AllRecipes.get(position).getRecipeName());
-                //startDetailActivityIntent.putExtra("ingridients", AllRecipes.get(position).getIngredients());
-                //startDetailActivityIntent.putExtra("steps", AllRecipes.get(position).getSteps());
-                //startDetailActivityIntent.putExtra("servings", AllRecipes.get(position).getServings());
-                //startDetailActivityIntent.putExtra("image", AllRecipes.get(position).getImage());
-                startActivity(startDetailActivityIntent);
-            }
-        });*/
 
     }
 
     @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+    }
+
+    @Override
     public void onListItemClick(RecipeModel clickedItemIndex) {
-        //Toast.makeText(this, "You clicked " + recipeAdapter.getItem(position) + " on row number " + position, Toast.LENGTH_SHORT).show();
         Log.v(TAG, "onListItemClick()!");
         Bundle recipeBundle = new Bundle();
         ArrayList<RecipeModel> recipeModel = new ArrayList<>();
         recipeModel.add(clickedItemIndex);
-        recipeBundle.putParcelableArrayList("recipe", recipeModel);
+        recipeBundle.putParcelableArrayList(SELECTED_RECIPES, recipeModel);
         final Intent recipeIntent = new Intent(this, DetailActivity.class);
         recipeIntent.putExtras(recipeBundle);
         startActivity(recipeIntent);
     }
-
-
 }
